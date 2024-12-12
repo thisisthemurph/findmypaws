88130c7b516ab53533de11767c24bff9511af60d
Mike Murphy <michael.w.murphy@outlook.com>
2024-12-12 06:13:55 +0000

    Remove unused var

--- client/src/pages/chat/ChatPage.tsx
+++ client/src/pages/chat/ChatPage.tsx
@@ -30,7 +30,6 @@ export default function ChatPage() {
     emojiReact,
     messageCount,
     isLoaded: isChatLoaded,
-    isTyping,
     otherParticipantIsTyping,
     handleTypingDetection,
   } = useChat(conversationIdentifier!);
